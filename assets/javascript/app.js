a3b8df236dac61f4a2f9103f261103ac660d2f39
Andrew <xie.andrew2235@gmail.com>
2019-03-04 23:11:47 -0800

    created the files and folders, also added HTML sheet with bootstrap and all the standard sheets. the reason why I chose to use bootstrap is so that I can have a jumbotron with my caption, and also a form to take in user input for when he/she would like to add the GIFs for a certain athlete not given. i have also created an array of 12 athletes from various sports, which matches my sports theme

---
+++ assets/javascript/app.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+// array of athletes
+    var topics = ["Nolan Arenado", "Marshawn Lynch", "Gervonta Davis", "LeBron James", "Ray Lewis", "LaDainian Tomlinson", "Michael Vick", "Vince Carter", "Andrelton Simmons", "Shaun White", "Devin Hester", "Muhammed Ali"];
