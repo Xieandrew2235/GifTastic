36e92f7fa7e66b7b9c78dbb2d8cab73e2d7c6a01
Andrew <xie.andrew2235@gmail.com>
2019-03-05 22:52:00 -0800

    pseudocoded how i was going to go about with this homework; fixed up the variables and added apiKey, set parameters and generated buttons for array. commented in a todo list

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,27 @@
-$(document).ready(function() {
-// array of athletes
-    var topics = ["Nolan Arenado", "Marshawn Lynch", "Gervonta Davis", "LeBron James", "Ray Lewis", "LaDainian Tomlinson", "Michael Vick", "Vince Carter", "Andrelton Simmons", "Shaun White", "Devin Hester", "Muhammed Ali"];
+// An array of athletes, which will be used to generate buttons dynamically.
+
+var peopleAthletic = ["lebron james", "ray lewis", "nolan arenado", "andrelton simmons", "kobe bryant", "buster posey"];
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=";
+var apiKey = "44EuboGZ4VXC2srzqR4cEZnqI0FD61th";
+var searchQty = 10;
+
+// Function to create button elements and display them on the top of the page.
+function buttonMaker() {
+  $("#athletics-buttons").empty();
+  for (i = 0; i <  peopleAthletic.length; i++) {
+    var emptyButton = $("<button>");
+    $(emptyButton).addClass("btn btn-primary m-1 legends-button");
+    $(emptyButton).attr("id", peopleAthletic[i]);
+    $(emptyButton).html(peopleAthletic[i]);
+    $("#athletics-buttons").append(emptyButton);
+  }
+};
+buttonMaker();
+
+// to-do list
+// create function to generate cards when athlete from array is clicked and/or if user inputs new athlete. also add in AJAX calls with parameters listed above
+// for loop + sources for still gifs and running gifs
+// user-submitted names: create new variable
+// event listeners
+
+
